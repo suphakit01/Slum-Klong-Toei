6d93f76441fbffac89d23bd3eadf084c06f8d7e3
suphakit01 <43027793+suphakit01@users.noreply.github.com>
2020-10-27 04:14:58 +0700

    ...

--- script.js
+++ script.js
@@ -11,3 +11,50 @@ window.onscroll = function() {
     }
     prevScrollpos = currentScrollPos;
 }
+
+// Parallax Scrolling Page1
+
+// layer1
+var layer1 = document.getElementById('layer1')
+scroll = window.pageYOffset;
+
+document.addEventListener('scroll',
+    function(e) {
+        var offset = window.pageYOffset;
+        scroll = offset;
+        layer1.style.width = (100 + scroll / 5) + '%';
+        layer1.style.right = scroll / 50 + '%';
+    });
+
+// layer2
+var layer2 = document.getElementById('layer2')
+scroll = window.pageYOffset;
+
+document.addEventListener('scroll',
+    function(e) {
+        var offset = window.pageYOffset;
+        scroll = offset;
+        layer2.style.width = (100 + scroll / 5) + '%';
+    });
+
+// layer3
+var layer3 = document.getElementById('layer3')
+scroll = window.pageYOffset;
+
+document.addEventListener('scroll',
+    function(e) {
+        var offset = window.pageYOffset;
+        scroll = offset;
+        layer3.style.top = -scroll / 20 + '%';
+    });
+
+// layer4
+var layer4 = document.getElementById('layer4')
+scroll = window.pageYOffset;
+
+document.addEventListener('scroll',
+    function(e) {
+        var offset = window.pageYOffset;
+        scroll = offset;
+        layer4.style.width = (100 + scroll / 5) + '%';
+    });
