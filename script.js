bf1aa76b2982a00db6bc3a3e7542f26e6b917c3b
suphakit01 <43027793+suphakit01@users.noreply.github.com>
2020-10-27 08:17:58 +0700

    ...

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-// Hide Navbar on Scroll Down and Show on Scroll Up
+//---------------------------------------------Hide Navbar on Scroll Down and Show on Scroll Up
 
 var prevScrollpos = window.pageYOffset;
 
 window.onscroll = function() {
+    let header = document.querySelector('header');
     var currentScrollPos = window.pageYOffset;
+
     if (prevScrollpos > currentScrollPos) {
         document.getElementById("site-header").style.top = "0";
-    } else {
+        header.classList.add('scrolling-active', );
+    } else if (prevScrollpos < currentScrollPos) {
         document.getElementById("site-header").style.top = "-8vh";
+    } else {
+        header.classList.remove('scrolling-active', );
     }
     prevScrollpos = currentScrollPos;
 }
 
-// Parallax Scrolling Page1
+//---------------------------------------------Scrolling Active Navbar
+
+// window.addEventListener('scroll', function() {
+//     let header = document.querySelector('header');
+//     let windowPosition = window.scrollY > 0;
+
+//     header.classList.toggle('scrolling-active');
+// })
+
+
+//---------------------------------------------Parallax Scrolling Page1
 
 // layer1
 var layer1 = document.getElementById('layer1')
@@ -34,19 +49,19 @@ document.addEventListener('scroll',
     function(e) {
         var offset = window.pageYOffset;
         scroll = offset;
-        layer2.style.width = (100 + scroll / 5) + '%';
+        layer2.style.width = (100 + scroll / 3.5) + '%';
     });
 
 // layer3
-var layer3 = document.getElementById('layer3')
-scroll = window.pageYOffset;
+// var layer3 = document.getElementById('layer3')
+// scroll = window.pageYOffset;
 
-document.addEventListener('scroll',
-    function(e) {
-        var offset = window.pageYOffset;
-        scroll = offset;
-        layer3.style.top = -scroll / 20 + '%';
-    });
+// document.addEventListener('scroll',
+//     function(e) {
+//         var offset = window.pageYOffset;
+//         scroll = offset;
+//         layer3.style.top = -scroll / 20 + '%';
+//     });
 
 // layer4
 var layer4 = document.getElementById('layer4')
@@ -56,5 +71,5 @@ document.addEventListener('scroll',
     function(e) {
         var offset = window.pageYOffset;
         scroll = offset;
-        layer4.style.width = (100 + scroll / 5) + '%';
+        layer4.style.width = (100 + scroll / 5.5) + '%';
     });
