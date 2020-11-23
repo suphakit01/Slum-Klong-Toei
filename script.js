0f6703795853bdb9fe7341105ec9c72826cee1fa
Suphakit Nch <43027793+suphakit01@users.noreply.github.com>
2020-11-23 17:31:08 +0700

    ...

--- script.js
+++ script.js
@@ -26,63 +26,6 @@ window.onscroll = function () {
     prevScrollpos = currentScrollPos;
 }
 
-//---------------------------------------------Scrolling Active Navbar
-
-// window.addEventListener('scroll', function() {
-//     let header = document.querySelector('header');
-//     let windowPosition = window.scrollY > 0;
-
-//     header.classList.toggle('scrolling-active');
-// })
-
-
-//-----------------------------------------------------------------------Parallax Scrolling Page1
-
-// layer1
-var layer1 = document.getElementById('layer1')
-scroll = window.pageYOffset;
-
-document.addEventListener('scroll',
-    function (e) {
-        var offset = window.pageYOffset;
-        scroll = offset;
-        layer1.style.width = (100 + scroll / 5) + '%';
-        layer1.style.right = scroll / 50 + '%';
-    });
-
-// layer2
-var layer2 = document.getElementById('layer2')
-scroll = window.pageYOffset;
-
-document.addEventListener('scroll',
-    function (e) {
-        var offset = window.pageYOffset;
-        scroll = offset;
-        layer2.style.width = (100 + scroll / 3.5) + '%';
-    });
-
-// layer3
-// var layer3 = document.getElementById('layer3')
-// scroll = window.pageYOffset;
-
-// document.addEventListener('scroll',
-//     function(e) {
-//         var offset = window.pageYOffset;
-//         scroll = offset;
-//         layer3.style.top = -scroll / 20 + '%';
-//     });
-
-// layer4
-var layer4 = document.getElementById('layer4')
-scroll = window.pageYOffset;
-
-document.addEventListener('scroll',
-    function (e) {
-        var offset = window.pageYOffset;
-        scroll = offset;
-        layer4.style.width = (100 + scroll / 3.5) + '%';
-    });
-
 //-----------------------------------------------------------------------Scrolling Fade-in / Fade-out
 
 window.onload = function () {
@@ -116,3 +59,76 @@ window.onload = function () {
     scrollEffect();
 
 }
+
+
+// l,d,x
+function castParallax() {
+
+	var opThresh = 350;
+	var opFactor = 750;
+
+	window.addEventListener("scroll", function(event){
+
+		var top = this.pageYOffset;
+
+		var layers = document.getElementsByClassName("parallax");
+		var layer, speed, yPos;
+		for (var i = 0; i < layers.length; i++) {
+			layer = layers[i];
+			speed = layer.getAttribute('data-speed');
+			var yPos = -(top * speed / 100);
+			layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
+
+		}
+	});
+}
+
+document.body.onload = castParallax();
+
+
+//-----------------------------------------------------------------------Parallax Scrolling Page1
+
+// // layer1
+// var layer1 = document.getElementById('layer1')
+// scroll = window.pageYOffset;
+
+// document.addEventListener('scroll',
+//     function (e) {
+//         var offset = window.pageYOffset;
+//         scroll = offset;
+//         layer1.style.width = (100 + scroll / 5) + '%';
+//         layer1.style.right = scroll / 50 + '%';
+//     });
+
+// // layer2
+// var layer2 = document.getElementById('layer2')
+// scroll = window.pageYOffset;
+
+// document.addEventListener('scroll',
+//     function (e) {
+//         var offset = window.pageYOffset;
+//         scroll = offset;
+//         layer2.style.width = (100 + scroll / 3.5) + '%';
+//     });
+
+// // layer3
+// var layer3 = document.getElementById('layer3')
+// scroll = window.pageYOffset;
+
+// document.addEventListener('scroll',
+//     function(e) {
+//         var offset = window.pageYOffset;
+//         scroll = offset;
+//         layer3.style.top = -scroll / 20 + '%';
+//     });
+
+// // layer4
+// var layer4 = document.getElementById('layer4')
+// scroll = window.pageYOffset;
+
+// document.addEventListener('scroll',
+//     function (e) {
+//         var offset = window.pageYOffset;
+//         scroll = offset;
+//         layer4.style.width = (100 + scroll / 3.5) + '%';
+//     });
